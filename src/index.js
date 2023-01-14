b8ff85c3d33e392a4e9bd5e7d9f29e7d4f319cc4
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-01-14 01:03:33 -0700

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -5,11 +5,3 @@ exports.handler = async (event) => {
   };
 };
 
-// exports.handler = async (event) => {
-//     // TODO implement
-//     const response = {
-//         statusCode: 200,
-//         body: JSON.stringify('Hello from Lambda!'),
-//     };
-//     return response;
-// };
