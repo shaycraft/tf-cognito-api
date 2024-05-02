62e78d30f60fb10d11a183b54eb8254bbf33ab5a
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2024-05-02 15:47:28 -0600

    chore: adds output values

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -2,3 +2,10 @@ output "user_pool_arn" {
   value = aws_cognito_user_pool.main.arn
 }
 
+output "token_url" {
+  value = aws_cognito_user_pool.main.endpoint
+}
+
+output "domain" {
+  value = aws_cognito_user_pool.main.domain
+}
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "arn" {
 output "api_endpoint" {
   value = aws_api_gateway_deployment.my_deployment.invoke_url
 }
+
+output "token_url" {
+  value = module.cognito.token_url
+}
+
+output "cognito_domain" {
+  value = module.cognito.domain
+}
