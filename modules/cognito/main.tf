f7a788619d13815288febb587dc353aca096437f
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2024-05-02 20:23:20 -0600

    refactor: variable renames

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -1,9 +1,9 @@
 resource "aws_cognito_user_pool" "main" {
-  name = "my_user_pool"
+  name = "tf_user_pool_poc"
 }
 
 resource "aws_cognito_user_pool_client" "main" {
-  name         = "my_user_pool_client"
+  name         = "tf_user_pool_client_poc"
   user_pool_id = aws_cognito_user_pool.main.id
 
   allowed_oauth_flows  = ["client_credentials"]
