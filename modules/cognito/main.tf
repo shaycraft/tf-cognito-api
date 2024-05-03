eda703516942ce1716b38833c115ebc63491c0cc
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2024-05-03 02:11:38 -0600

    fix: finally working, had to add custom domain

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -7,7 +7,7 @@ resource "aws_cognito_user_pool_client" "main" {
   user_pool_id = aws_cognito_user_pool.main.id
 
   allowed_oauth_flows                  = ["client_credentials"]
-  allowed_oauth_scopes                 = ["ev/read"]
+  allowed_oauth_scopes                 = ["poc/read"]
   allowed_oauth_flows_user_pool_client = true
 
   explicit_auth_flows = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH",
@@ -15,11 +15,21 @@ resource "aws_cognito_user_pool_client" "main" {
 
 
   generate_secret = true
+
+  access_token_validity  = 12
+  id_token_validity      = 12
+  refresh_token_validity = 1
+
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
 }
 
 resource "aws_cognito_resource_server" "cognito_resource_server" {
-  identifier = "ev"
-  name       = "ev"
+  identifier = "poc"
+  name       = "tf_resource_server_poc"
 
   user_pool_id = aws_cognito_user_pool.main.id
 
@@ -27,4 +37,9 @@ resource "aws_cognito_resource_server" "cognito_resource_server" {
     scope_description = "POC scope"
     scope_name        = "read"
   }
+}
+
+resource "aws_cognito_user_pool_domain" "cognito_domain" {
+  domain       = "tf-ev-poc-sso"
+  user_pool_id = aws_cognito_user_pool.main.id
 }
